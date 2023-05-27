a92fb4fcdb16dfc4fdbe1205cd7f86f6fb70a309
MarcusRosaa <marcusvrosa1@gmail.com>
2023-05-27 13:55:00 -0300

    feat: update link automatically

--- src/components/ButtonMessage/index.tsx
+++ src/components/ButtonMessage/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Container } from './styles';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '../../firebase/firebase';
 
 const ButtonMessage: React.FC = () => {
+  const [link, setLink] = useState('');
+
+  useEffect(() => {
+    const fetchLink = async () => {
+      try {
+        const linkDocRef = doc(db, 'Link', 'mYFfzOWVhdcl12F6SCu1');
+        const linkDocSnap = await getDoc(linkDocRef);
+        if (linkDocSnap.exists()) {
+          setLink(linkDocSnap.data().url);
+        }
+      } catch (error) {
+        console.log('Error fetching link:', error);
+      }
+    };
+
+    fetchLink();
+  }, []);
+  console.log(link)
   return (
     <Container>
-      <a href="https://checkout.perfectpay.com.br/pay/PPU38CM4EIM">Ver mensagens suspeitas agora</a>
+      <a href={link}>Ver mensagens suspeitas agora</a>
     </Container>
-  )
-}
+  );
+};
 
 export default ButtonMessage;
